a026b1edcc0be6fc9206ab202396b939aa75ded4
Argelia <argelia.garcia.g@gmail.com>
2014-08-25 22:27:37 -0500

    ahora este

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,9 +3,9 @@ package com.test;
 public class Test{
 	public static void main(String[] args){
 		//No recuerdo los nombres en inglés jaja
-		PowerPuffGirl bombon = new PowerPuffGirl("Bombon");
-		PowerPuffGirl burbuja = new PowerPuffGirl("burbuja"); //De ésta sí, peor pos ya!
-		PowerPuffGirl bellota = new PowerPuffGirl("Bellota");
+		PowerPuffGirl bombon = new PowerPuffGirl("Bombon"); //era la roja
+		PowerPuffGirl burbuja = new PowerPuffGirl("burbuja"); //De ésta sí, peor pos ya! Era la azul
+		PowerPuffGirl bellota = new PowerPuffGirl("Bellota"); //era la verde :v
 
 		System.out.println(bombon.getNameComplete());
 		System.out.println(burbuja.getNameComplete());
