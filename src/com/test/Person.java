c74131d524bc684d75d5d883648a4320bcafc4c8
Erick Martinez <s_e_b_a_s_1010@hotmail.com>
2014-08-25 20:13:56 -0500

    Clase Person

---
+++ src/com/test/Person.java
@@ -0,0 +1,51 @@
+package com.test;
+
+public class Person{
+	private int age;
+	private String name;
+	private String lastName;
+	private String firstName;
+
+	public Person(String n,String f, String l,int a){
+		this.age = a;
+		this.name = n;
+		this.firstName = f;
+		this.lastName = l;
+	}
+
+	public int getAge(){
+		return age;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getLastName(){
+		return lastName;
+	}
+
+	public String getFirstName(){
+		return firstName;
+	}
+
+	public String getNameComplete(){
+		return name + " "+firstName + " "+ lastName;
+	}
+
+	public void setAge(int age){
+		this.age = age;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+}
